3dfaa8d57f43870ccf0c136062b46ee8a2878eaf
Nigel Smith <nigsmith@redhat.com>
2025-12-04 10:32:39 +0000

    adding the launcher script

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # must-gather_gather_metrics
+
+`oc adm must-gather` can now collect metrics from a given cluster
+
+```
+oc adm must-gather -- gather_metrics \
+--min-time=$(date --date='2 hours ago' +%s%3N) \
+--match="{__name__=~\'kube_node_.*\'}"
+```
+
+Run the must-gather command within this repo or place a pre collected `must-gather -- gather_metrics` tar into the root of this repo 
+Running the launcher will result in the collected data being available via a running container
+
+```
+./launcher.sh
+```
---
+++ launcher.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#Clean up
+rm -rf must-*
+rm -rf tmp/*
+
+#Get desired PROM_VERSION
+echo -n "insert desired Prometheus version - example '2.45.0' :"
+read PROM_VERSION
+
+#Download the desired PROM_VERSION
+curl -Ls https://github.com/prometheus/prometheus/releases/download/v$PROM_VERSION/prometheus-$PROM_VERSION.linux-amd64.tar.gz | tar -xvz -C tmp
+
+#Create configfiles
+mkdir tmp/prometheus-config
+touch tmp/prometheus-config/prometheus.yml
+
+#Create tsdb blocks from openmetrics 
+tmp/prometheus-2.45.0.linux-amd64/promtool tsdb create-blocks-from openmetrics must-gather.local.*/quay-io-openshift-*/monitoring/metrics/metrics.openmetrics tmp/prometheus-2.45.0.linux-amd64/data/
+
+#Launch the container
+podman run --rm -p 9090:9090/tcp -v $PWD/tmp/prometheus-2.45.0.linux-amd64/data:/prometheus:U,Z --privileged quay.io/prometheus/prometheus --storage.tsdb.path=/prometheus --config.file=/dev/null
