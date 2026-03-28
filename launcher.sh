ab8c0b0bc3c2b0e498641ce89092d7706890d83b
Nigel Smith <nigsmith@redhat.com>
2026-03-28 13:14:49 +0000

    start to add perses dashboards

--- launcher.sh
+++ launcher.sh
@@ -29,6 +29,20 @@ touch tmp/prometheus-config/prometheus.yml
 echo "creating TSDB blocks...."
 tmp/prometheus-$PROM_VERSION.linux-amd64/promtool tsdb create-blocks-from openmetrics must-gather.local.*/quay-io-openshift-*/monitoring/metrics/metrics.openmetrics tmp/prometheus-$PROM_VERSION.linux-amd64/data/
 
-#Launch the container
+#Launch Prometheus in the background
 echo "launching the Prometheus instance..."
-podman run --rm -p 9090:9090/tcp -v $PWD/tmp/prometheus-$PROM_VERSION.linux-amd64/data:/prometheus:U,Z --privileged quay.io/prometheus/prometheus:v$PROM_VERSION --storage.tsdb.path=/prometheus --config.file=/dev/null
+PROM_CONTAINER=$(podman run --rm -d -p 9090:9090/tcp -v $PWD/tmp/prometheus-$PROM_VERSION.linux-amd64/data:/prometheus:U,Z --privileged quay.io/prometheus/prometheus:v$PROM_VERSION --storage.tsdb.path=/prometheus --config.file=/dev/null)
+echo "Prometheus running (container: $PROM_CONTAINER)"
+echo "Browse Prometheus at http://localhost:9090"
+
+#Stop Prometheus when the script exits
+trap "echo 'stopping Prometheus...'; podman stop $PROM_CONTAINER" EXIT
+
+#Launch Perses (foreground)
+echo "launching Perses..."
+echo "Browse Perses at http://localhost:8080"
+podman run --rm --network=host \
+  -v $PWD/perses/config.yaml:/etc/perses/config.yaml:Z \
+  -v $PWD/perses/provisioning:/etc/perses/provisioning:Z \
+  persesdev/perses \
+  --config=/etc/perses/config.yaml
